267bf2bcf461c01d490df14386256f7700cf4f82
Jaldhi Shukla <jaldhishukla7@gmail.com>
2026-04-20 10:52:39 +0530

    Set search_path to Public in DB pool

--- src/db.ts
+++ src/db.ts
@@ -69,7 +69,8 @@ const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-  }
+  },
+  options: '-c search_path=public'
 });
 
 pool.on('connect', () => {
